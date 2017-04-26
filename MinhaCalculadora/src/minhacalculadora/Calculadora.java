fa2df551a6602022bc07eb00bc58679d1e8d85e4
TECNICO <TECNICO@LAB204BPC20>
2017-04-25 21:24:13 -0300

    Criação da classe calculadora

---
+++ MinhaCalculadora/src/minhacalculadora/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minhacalculadora;
+
+/**
+ *
+ * @author TECNICO
+ */
+public class Calculadora {
+    
+    public double soma(){
+        
+        return 3+5;
+    }
+    
+}
